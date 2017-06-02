6b80a4686405150f1ed95fde7f74bf9921e5c77e
flangel <fll@scade.io>
2017-06-03 00:03:00 +0200

    Added UgTilesMenuDemo

---
+++ UgTilesMenuDemo/src/main.page.swift
@@ -0,0 +1,20 @@
+import ScadeKit
+
+class MainPageAdapter: SCDLatticePageAdapter {
+
+	// page adapter initialization
+	override func load(_ path: String) {		
+		super.load(path)
+		
+		[1,2,3,4,5,6,7].forEach{
+			let name = "verticalview\($0)";
+			if let clickable = self.page!.getWidgetByName(name) as? SCDWidgetsClickable {
+				clickable.onClick.append(SCDWidgetsEventHandler{ _ in self.menuClicked(name)})
+			}
+		}
+	}
+	
+	func menuClicked(_ id:String) {
+		print("Menu \(id) clicked")
+	}
+}
---
+++ UgTilesMenuDemo/src/start.swift
@@ -0,0 +1,21 @@
+#if os(Linux) || os(Android)
+import SwiftFoundation
+#endif
+import ScadeKit
+
+@objc class Main : ObjectiveC.NSObject, SCDApplication {
+
+ 	let moduleName  = "UgTilesMenuDemo"
+	var mainView: SCDLatticeView!
+  	var mainAdapter: MainPageAdapter!
+  	
+	func main() {
+		SCDRuntime.initRuntime(self)
+
+		mainView = SCDLatticeView()
+		mainAdapter = MainPageAdapter()
+		
+		mainAdapter.load("main.page")
+		mainAdapter.show(mainView)
+	}
+}
