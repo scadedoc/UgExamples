ecf063c7d10689cb6f1dafbd5c1f20247dcda13f
bulantsevajo <bulantsevajo@gmail.com>
2017-11-02 17:50:52 +0700

    Update examples.

--- UgListControlDemo/src/Dog.swift
+++ UgListControlDemo/src/Dog.swift
@@ -4,10 +4,10 @@ class Dog : EObject {
 	
 	// please make sure you annotate each variable with the type
 	// for SCADE to more identify and leverage the type information
-	let id : String
-	let name : String
-	let ageInYears : Int 
-	let breed:String 
+	@objc dynamic var id : String
+	@objc dynamic var name : String
+	@objc dynamic var ageInYears : Int 
+	@objc dynamic var breed:String 
 	
 	init(id:String,name:String,breed:String,ageInYears:Int) {
 		self.id = id
