b80f459681b5f469939d64d81f8a8cc77b0f04e4
flangel <fll@scade.io>
2019-09-19 22:45:42 +0200

    Fixing UgCalculator example

--- UgCalculator/src/main.page.swift
+++ UgCalculator/src/main.page.swift
@@ -1,35 +1,70 @@
 import ScadeKit
-
+import Expression
 
 class MainPageAdapter: SCDLatticePageAdapter {
 	
+	// variable buffer is used a model for model-view binding
 	@objc dynamic var buffer:String = "0"
+	
 	var isOperatorMode = false
+	
+	let colorNumberKeyDefault = SCDGraphicsRGB(red:100,green:101,blue:104) // "646568" // dark gray
+	let colorNumberKeyDown = SCDGraphicsRGB(red:192,green:192,blue:192) // magnesiumlightGray
+	let colorOtherkeyDefault = SCDGraphicsRGB(red:69,green:71,blue:	76)
+	let colorOtherkeyDown = SCDGraphicsRGB(red:100,green:101,blue:104) 
+	let colorOperatorKeyDefault = SCDGraphicsRGB(red:255,green:158,blue:11)
+	let colorOperatorKeyDown = SCDGraphicsRGB(red:216,green:133,blue:1)
+	
+	
+	// Button ids and input
+	lazy var  buttonNameInputs = 
+			[("btnNumber1", ("1", colorNumberKeyDefault, colorNumberKeyDown) ), 
+			 ("btnNumber2", ("2", colorNumberKeyDefault, colorNumberKeyDown) ), 
+ 			 ("btnNumber3", ("3", colorNumberKeyDefault, colorNumberKeyDown) ), 
+ 			 ("btnNumber4", ("4", colorNumberKeyDefault, colorNumberKeyDown) ), 
+ 			 ("btnNumber5", ("5", colorNumberKeyDefault, colorNumberKeyDown) ), 
+ 			 ("btnNumber6", ("6", colorNumberKeyDefault, colorNumberKeyDown) ), 
+ 			 ("btnNumber7", ("7", colorNumberKeyDefault, colorNumberKeyDown) ), 
+ 			 ("btnNumber8", ("8", colorNumberKeyDefault, colorNumberKeyDown) ), 
+ 			 ("btnNumber9", ("9", colorNumberKeyDefault, colorNumberKeyDown) ), 
+ 			 ("btnNumber0", ("0", colorNumberKeyDefault, colorNumberKeyDown) ), 
+ 			 
+			 ("btnPlus",("+", colorOperatorKeyDefault,colorOperatorKeyDown) ),
+			 ("btnMinus",("-", colorOperatorKeyDefault,colorOperatorKeyDown) ),
+			 ("btnDivide",("/", colorOperatorKeyDefault,colorOperatorKeyDown) ),
+			 ("btnMultiple",("*", colorOperatorKeyDefault,colorOperatorKeyDown) ),
+			 ("btnEqual",("=", colorOperatorKeyDefault,colorOperatorKeyDown) ),
+			 
+			 ("btnClear",("A",colorOtherkeyDefault,colorOtherkeyDown) ),
+			 ]
 	
 	// page adapter initialization
 	override func load(_ path: String) {		
 		super.load(path)
 		
-		self.setupButtons()
+		// send specific input for each button
+		self.setupButtonActionsAndEffect()
+		
 	}
 	
 	func solveEqualtion(equation:String) -> String {
-		let mathExpression = NSExpression(format: equation) 
-		if let mathValue = mathExpression.expressionValue(with: nil, context: nil)  {
-			return String(describing:mathValue)
+		let expression = Expression(equation)
+		if let result = try? expression.evaluate()  { 
+			return String(Int(result))
 		}
 		return "Error"
 	}
 	
-	func byThree(str:String) -> String {
+	func shortenByThree(str:String) -> String {
 		let endIndex = str.index(str.endIndex, offsetBy:-3)
 		let x = str[str.startIndex..<endIndex]
 		return String(x)
 	}
 	
+	// This is the business logic to process the calculator input, display the input and
+	// and compute results. This logic is for demonstration purposes  and not meant for production use. 
 	func process(_ input:String) {
-		print(input)
-		
+
 		let isNumber = ["1","2","3","4","5","6","7","8","9","0"].contains(input) 
 		if(isNumber) {
 			
@@ -42,7 +77,7 @@ class MainPageAdapter: SCDLatticePageAdapter {
 		let isOperator = ["+","-","/","*"].contains(input) 
 		if(isOperator) {
 			if(isOperatorMode) {
-				buffer = byThree(str: buffer) + " \(input) "
+				buffer = shortenByThree(str: buffer) + " \(input) "
 				return
 			}
 			buffer = buffer + " \(input) "
@@ -63,55 +98,33 @@ class MainPageAdapter: SCDLatticePageAdapter {
 		
 	}
  
-	
-	func setupButtons(){ 
+	func setupButtonActionsAndEffect(){ 
 		
-		let colorNumberDown = SCDGraphicsRGB(red:192,green:192,blue:192) // magnesiumlightGray
-		let ColorNumberUp = SCDGraphicsRGB(red:100,green:101,blue:104) // "646568" // dark gray
-		let operatorUp = "FF9E0B" // orange
-		let operatorDown = "E18C0B" // gray orange
-		
-		let buttonNameInputs = 
-			[("btnNumber1", "1"), // colorNumberDown ) ),
-			 ("btnNumber2", "2"), // ColorNumberUp ) ),
-			 ("btnNumber3","3"),
-			 ("btnNumber4","4"),
-			 ("btnNumber5","5"),
-			 ("btnNumber6","6"),
-			 ("btnNumber7","7"),
-			 ("btnNumber8","8"),
-			 ("btnNumber9","9"),
-			 ("btnNumber0","0"),
-			 ("btnPlus","+"),
-			 ("btnMinus","-"),
-			 ("btnDivide","/"),
-			 ("btnMultiple","*"),
-			 ("btnEqual","="),
-			 ("btnClear","A"),
-			 ]
+		// 1. change background color when pressed. 2. link actions to buttons
+		for (btnName,input) in self.buttonNameInputs {
 			
-		for (btnName,input) in buttonNameInputs {
-			if let btn = self.page!.getWidgetByName(btnName) as? SCDWidgetsButton {
-				
-				print("init")
-				
-				btn.onClick.append( SCDWidgetsEventHandler {
-					_ in 
-						print(" hello \(input) ")
-						self.process(input)
-				})
-						
-				 let gestureHandler =  SCDSvgTapGestureRecognizer{ tap in 
-					//let tapEvent = tap as! SCDSvgTapGestureRecognizer
-					print("tabbed")
-				 }
-				 // set number of taps after which the gesture event is fired
-				 gestureHandler.numTaps = 1
-			//	btn.drawing!.gestureRecognizers.append(gestureHandler)
-						
+			// get button reference, but cast it to higher class SCDWidgetsWidget,
+			// because the 0 button is not button, but a container and 
+			// SCDWidgetsWidget represents both
+			if let btn = self.page!.getWidgetByName(btnName) as? SCDWidgetsWidget {
+ 
+				// create gesture
+				let gestureHandler = SCDSvgPanGestureRecognizer{ tap in 
+					print("gesture handler")
+					switch(tap!.state) {
+						case .began:
+							btn.backgroundColor = input.2 // pressed color
+						case .ended:
+							btn.backgroundColor = input.1 // default color
+							self.process(input.0)  // process input
+						default: 
+							return
+					}
 				}
-			}
-			
+				// Add gesture to button
+			    btn.drawing!.gestureRecognizers.append(gestureHandler)
+		    }
+		} 
 	}
 	
 }
