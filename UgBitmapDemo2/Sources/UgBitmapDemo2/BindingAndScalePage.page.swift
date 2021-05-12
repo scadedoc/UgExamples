f033d8155b8f2eff40bf1ed310afebc3ab36a328
Julia Bulantseva <bulantsevajo@gmail.com>
2021-05-12 23:04:10 +0700

    Updated UgBitmap example.

--- UgBitmapDemo2/Sources/UgBitmapDemo2/BindingAndScalePage.page.swift
+++ UgBitmapDemo2/Sources/UgBitmapDemo2/BindingAndScalePage.page.swift
@@ -6,9 +6,9 @@ class BindingAndScalePagePageAdapter: SCDLatticePageAdapter {
   override func load(_ path: String) {
     super.load(path)
     
-    self.image1.url = "Assets/dog2.jpg"
-    self.image1.contentPriority = false
+    image1.url = "Assets/dog2.jpg"
+    image1.contentPriority = false
     
-	self.main.onClick.append(SCDWidgetsEventHandler{_ in self.navigation!.go(page: "main.page")})
+	main.onClick{_ in self.navigation!.go(page: "main.page")}
   }
 }
