df1d36583a0c57e666bafc174318e98e16f1c920
Julia Bulantseva <bulantsevajo@gmail.com>
2021-05-13 23:13:45 +0700

    Updated Kyle1 example.

--- Kyle1/Sources/Kyle1/main.page.swift
+++ Kyle1/Sources/Kyle1/main.page.swift
@@ -7,21 +7,16 @@ let whiteColor = SCDGraphicsRGB(red:241,green:241,blue:241)
 let orangeColor = SCDGraphicsRGB(red:255,green:127,blue:80)
 
 class MainPageAdapter: SCDLatticePageAdapter {
-
-	var grid : SCDWidgetsGridView?
 	
 	// page adapter initialization
 	override func load(_ path: String) {		
 		super.load(path)
 		
-		// get grid
-		self.grid =  self.page!.getWidgetByName("grid1") as? SCDWidgetsGridView
-		
 		// set matrix size
 		let (columns,rows) = (UInt(20),UInt(4))
 		
 		// set grid size
-		self.grid!.setGridSize(to:(columns,rows))
+		grid1.setGridSize(to:(columns,rows))
 		
 		// populate grid
 		self.populateGrid(columns:columns,rows:rows)
@@ -31,7 +26,7 @@ class MainPageAdapter: SCDLatticePageAdapter {
 		for c in (0..<columns) {
 			for r in (0..<rows) {
 				let button = createButton(label:"X", action:{ e in print(e) })
-				 self.grid!.insert(button,at:(c,r))
+				 grid1.insert(button,at:(c,r))
 			}
 		}
 	}
@@ -44,10 +39,10 @@ class MainPageAdapter: SCDLatticePageAdapter {
 	  button.text = "x"
 	  
 	  // on click, call callback with name of button
-	  button.onClick.append( SCDWidgetsEventHandler{ (e:SCDWidgetsEvent?) in 
-		  let button = e!.target as! SCDWidgetsButton
-		  action(button.name) 
-		})
+	  button.onClick{ (e:SCDWidgetsEvent?) in 
+		 let button = e!.target as! SCDWidgetsButton
+		 action(button.name) 
+	 }
 	  
 	  // set colors
 	  button.font!.color = whiteColor
