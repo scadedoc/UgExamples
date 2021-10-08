0bb733f68593f584f83e6016d54f09e0991ecd6a
Julia Bulantseva <bulantsevajo@gmail.com>
2021-10-08 22:00:05 +0700

    Updated pages.

---
+++ SimplyE/Sources/SimplyE/Extensions/Pages/settings.page.ext.swift
@@ -0,0 +1,203 @@
+import ScadeKit
+
+extension SettingsPageAdapter {
+  var viewcenter: SCDWidgetsListView {
+    return self.page?.getWidgetByName("viewcenter") as! SCDWidgetsListView
+  }
+
+  var rowview1: SCDWidgetsListView {
+    return self.page?.getWidgetByName("rowview1") as! SCDWidgetsListView
+  }
+
+  var horizontalview2: SCDWidgetsRowView {
+    return self.page?.getWidgetByName("horizontalview2") as! SCDWidgetsRowView
+  }
+
+  var label1: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("label1") as! SCDWidgetsLabel
+  }
+
+  var label2: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("label2") as! SCDWidgetsLabel
+  }
+
+  var rowview11: SCDWidgetsListView {
+    return self.page?.getWidgetByName("rowview11") as! SCDWidgetsListView
+  }
+
+  var horizontalview21: SCDWidgetsRowView {
+    return self.page?.getWidgetByName("horizontalview21") as! SCDWidgetsRowView
+  }
+
+  var label11: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("label11") as! SCDWidgetsLabel
+  }
+
+  var label21: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("label21") as! SCDWidgetsLabel
+  }
+
+  var verticalview23: SCDWidgetsListView {
+    return self.page?.getWidgetByName("verticalview23") as! SCDWidgetsListView
+  }
+
+  var rowview111: SCDWidgetsListView {
+    return self.page?.getWidgetByName("rowview111") as! SCDWidgetsListView
+  }
+
+  var subrow1: SCDWidgetsListView {
+    return self.page?.getWidgetByName("subrow1") as! SCDWidgetsListView
+  }
+
+  var horizontalview211: SCDWidgetsRowView {
+    return self.page?.getWidgetByName("horizontalview211") as! SCDWidgetsRowView
+  }
+
+  var label111: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("label111") as! SCDWidgetsLabel
+  }
+
+  var label211: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("label211") as! SCDWidgetsLabel
+  }
+
+  var subrow2: SCDWidgetsListView {
+    return self.page?.getWidgetByName("subrow2") as! SCDWidgetsListView
+  }
+
+  var horizontalview2111: SCDWidgetsRowView {
+    return self.page?.getWidgetByName("horizontalview2111") as! SCDWidgetsRowView
+  }
+
+  var label1111: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("label1111") as! SCDWidgetsLabel
+  }
+
+  var label2111: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("label2111") as! SCDWidgetsLabel
+  }
+
+  var subrow21: SCDWidgetsListView {
+    return self.page?.getWidgetByName("subrow21") as! SCDWidgetsListView
+  }
+
+  var horizontalview21111: SCDWidgetsRowView {
+    return self.page?.getWidgetByName("horizontalview21111") as! SCDWidgetsRowView
+  }
+
+  var label11111: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("label11111") as! SCDWidgetsLabel
+  }
+
+  var label21111: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("label21111") as! SCDWidgetsLabel
+  }
+
+  var subrow211: SCDWidgetsListView {
+    return self.page?.getWidgetByName("subrow211") as! SCDWidgetsListView
+  }
+
+  var horizontalview211111: SCDWidgetsRowView {
+    return self.page?.getWidgetByName("horizontalview211111") as! SCDWidgetsRowView
+  }
+
+  var label111111: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("label111111") as! SCDWidgetsLabel
+  }
+
+  var label211111: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("label211111") as! SCDWidgetsLabel
+  }
+
+  var topline1211111: SCDWidgetsCustomWidget {
+    return self.page?.getWidgetByName("topline1211111") as! SCDWidgetsCustomWidget
+  }
+
+  var topline121211: SCDWidgetsCustomWidget {
+    return self.page?.getWidgetByName("topline121211") as! SCDWidgetsCustomWidget
+  }
+
+  var verticalview1: SCDWidgetsListView {
+    return self.page?.getWidgetByName("verticalview1") as! SCDWidgetsListView
+  }
+
+  var horizontalview4: SCDWidgetsRowView {
+    return self.page?.getWidgetByName("horizontalview4") as! SCDWidgetsRowView
+  }
+
+  var horizontalview3: SCDWidgetsRowView {
+    return self.page?.getWidgetByName("horizontalview3") as! SCDWidgetsRowView
+  }
+
+  var horizontalview1: SCDWidgetsRowView {
+    return self.page?.getWidgetByName("horizontalview1") as! SCDWidgetsRowView
+  }
+
+  var label3: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("label3") as! SCDWidgetsLabel
+  }
+
+  var toolbar11: SCDWidgetsToolBar {
+    return self.page?.getWidgetByName("toolbar11") as! SCDWidgetsToolBar
+  }
+
+  var itmCatalog: SCDWidgetsToolBarItem {
+    return self.page?.getWidgetByName("itmCatalog") as! SCDWidgetsToolBarItem
+  }
+
+  var icon11: SCDWidgetsImage {
+    return self.page?.getWidgetByName("icon11") as! SCDWidgetsImage
+  }
+
+  var caption11: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("caption11") as! SCDWidgetsLabel
+  }
+
+  var item21: SCDWidgetsToolBarItem {
+    return self.page?.getWidgetByName("item21") as! SCDWidgetsToolBarItem
+  }
+
+  var icon21: SCDWidgetsImage {
+    return self.page?.getWidgetByName("icon21") as! SCDWidgetsImage
+  }
+
+  var caption21: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("caption21") as! SCDWidgetsLabel
+  }
+
+  var item31: SCDWidgetsToolBarItem {
+    return self.page?.getWidgetByName("item31") as! SCDWidgetsToolBarItem
+  }
+
+  var icon31: SCDWidgetsImage {
+    return self.page?.getWidgetByName("icon31") as! SCDWidgetsImage
+  }
+
+  var caption31: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("caption31") as! SCDWidgetsLabel
+  }
+
+  var item: SCDWidgetsToolBarItem {
+    return self.page?.getWidgetByName("item") as! SCDWidgetsToolBarItem
+  }
+
+  var icon41: SCDWidgetsImage {
+    return self.page?.getWidgetByName("icon41") as! SCDWidgetsImage
+  }
+
+  var caption41: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("caption41") as! SCDWidgetsLabel
+  }
+
+  var topline131: SCDWidgetsCustomWidget {
+    return self.page?.getWidgetByName("topline131") as! SCDWidgetsCustomWidget
+  }
+
+  var horizontalview5: SCDWidgetsRowView {
+    return self.page?.getWidgetByName("horizontalview5") as! SCDWidgetsRowView
+  }
+
+  var label4: SCDWidgetsLabel {
+    return self.page?.getWidgetByName("label4") as! SCDWidgetsLabel
+  }
+}
