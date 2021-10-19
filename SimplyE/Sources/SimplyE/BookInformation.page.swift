afb161be5ec7def2741a2a335954c32f44ebdf8a
shem <vsv@scade.io>
2021-10-19 19:21:42 +0200

    [SimelyE] fix BookInformation

--- SimplyE/Sources/SimplyE/BookInformation.page.swift
+++ SimplyE/Sources/SimplyE/BookInformation.page.swift
@@ -2,7 +2,7 @@ import ScadeKit
 
 class BookInformationPageAdapter: SCDLatticePageAdapter {
 
-  var book: Book?
+  //var book: Book?
   var trueAttribute: Bool = true
 
   // page adapter initialization
@@ -22,36 +22,30 @@ class BookInformationPageAdapter: SCDLatticePageAdapter {
   }
 
   func enterpage(ev: SCDWidgetsEnterEvent) {
-    if let isbn = ev.data as? String {
-      print("loading book \(isbn)")
-      self.book = BookManager.sharedInstance.getBook(isbn: isbn)!
+    guard let isbn = ev.data as? String,
+          let book = BookManager.sharedInstance.getBook(isbn: isbn),
+          let bookImageData = NetworkUtils.loadData(from: book.bookCoverUrl) else { return }
+    
+    print("loading book \(isbn)")
 
-      bmpBook.content = NetworkUtils.loadData(from: book!.bookCoverUrl)!
-      bmpBook.contentPriority = true
+    bmpBook.content = bookImageData
+    bmpBook.contentPriority = true
 
-      list.elementProvider = SCDWidgetsElementProvider { (book: Book, listElement) in
-
-        guard let lbDescription1 = listElement.getWidgetByName("lbDescription1") as? SCDWidgetsLabel else {
-          return
-        }
-        lbDescription1.text = book.description1
-        
-        guard let lbDescription2 = listElement.getWidgetByName("lbDescription2") as? SCDWidgetsLabel else {
-          return
-        }
-        lbDescription2.text = book.description2
-      }
-      list.items = [self.book]
-
-      //lbDescription1.text = book!.description1
-      //lbDescription2.text = book!.description2
-      lbBookTitle.text = book!.title
-      lbAuthorName.text = book!.author
-      lbPublisher.text = book!.publisher
-      lbPublished.text = book!.published
-      lbCategory.text = book!.category
-
+    list.elementProvider = SCDWidgetsElementProvider { (book: Book, elem) in
+      guard let label1 = elem.getWidgetByName("lbDescription1") as? SCDWidgetsLabel,
+            let label2 = elem.getWidgetByName("lbDescription2") as? SCDWidgetsLabel else { return }
+      label1.text = book.description1
+      label2.text = book.description2
     }
+    list.items = [book]
+
+    //lbDescription1.text = book!.description1
+    //lbDescription2.text = book!.description2
+    lbBookTitle.text = book.title
+    lbAuthorName.text = book.author
+    lbPublisher.text = book.publisher
+    lbPublished.text = book.published
+    lbCategory.text = book.category
   }
 
   func goBack() {
