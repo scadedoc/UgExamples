766f782d60d5cb9626285022534ef8679753bbfd
Cdf-creator <Olanrewajuolakunle10@gmail.com>
2023-07-07 06:42:28 +0100

    Code restructuring for about/contactUs.page.swift

--- SimplyE/Sources/SimplyE/settings/contactUs.page.swift
+++ SimplyE/Sources/SimplyE/settings/contactUs.page.swift
@@ -1,34 +1,35 @@
-import ScadeKit
 import Foundation
+import ScadeKit
 
 class ContactUsPageAdapter: SCDLatticePageAdapter {
+
+  let contactUsWebView: String =
+    "https://www.scade.io/account/login/?redirect_to=https://www.scade.io/account#"
 
   // page adapter initialization
   override func load(_ path: String) {
     super.load(path)
+
+    self.doneButton.onClick { _ in
+      Navigation.go(.settings, clearHistory: true)
+    }
+
+    self.reloadButton.onClick { _ in
+      self.webView.load(self.contactUsWebView)
+    }
+
   }
 
   override func show(view: SCDLatticeView?, data: Any) {
     super.show(view: view, data: data)
-    
-    self.doneButton.onClick { _ in
-    Navigation.go(.settings, clearHistory: true)
-    }
 
-    let contactUsWebView: String =
-      "https://www.scade.io/account/login/?redirect_to=https://www.scade.io/account#"
-      
-      guard let url = URL(string: contactUsWebView) else { return }
+    guard let url = URL(string: self.contactUsWebView) else { return }
 
-    guard let domain = url.host else {return}
+    guard let domain = url.host else { return }
 
     self.titleLabel.text = domain
 
-    webView.load(contactUsWebView)
-    
-    self.reloadButton.onClick { _ in
-        self.webView.load(contactUsWebView)
-      }
+    webView.load(self.contactUsWebView)
 
     webView.onLoaded.append(
       SCDWidgetsLoadEventHandler { event in
