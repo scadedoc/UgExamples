48e721cc8268aff31288513b4d6a35f5db3812ad
Julia Makeeva <bulantsevajo@gmail.com>
2024-09-30 23:06:19 -0300

    Updated UgGestureDemo.

--- UgGestureDemo/Package.swift
+++ UgGestureDemo/Package.swift
@@ -3,8 +3,6 @@
 import PackageDescription
 import Foundation
 
-let SCADE_SDK = ProcessInfo.processInfo.environment["SCADE_SDK"] ?? ""
-
 let package = Package(
     name: "UgGestureDemo",
     platforms: [
@@ -23,14 +21,13 @@ let package = Package(
       
     ],
     targets: [
+        .binaryTarget(name: "ScadeKit",
+                      url: "https://github.com/scade-platform/ScadeKit/releases/download/2.4.0/ScadeKit.xcframework.zip",
+                      checksum: "4bef943aa45155d3d25cad539710dec0c489d3ede287f8acdc5d5b4bc0f2d783"),
         .target(
             name: "UgGestureDemo",
-            dependencies: [],
-            exclude: ["main.page"],
-            swiftSettings: [
-                .unsafeFlags(["-F", SCADE_SDK], .when(platforms: [.macOS, .iOS])),
-                .unsafeFlags(["-I", "\(SCADE_SDK)/include"], .when(platforms: [.android])),
-            ]
+            dependencies: ["ScadeKit"],
+            exclude: ["main.page"]
         )
     ]
 )
